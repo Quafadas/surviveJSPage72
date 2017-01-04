03b29b9d34e49e938cd7dede26609a776106027b
Simon Parten <quafadas@gmail.com>
2017-01-04 12:58:11 +0100

    New error...

--- app/components/Provider/Provider.prod.jsx
+++ app/components/Provider/Provider.prod.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AltContainer from 'alt-container';
-import alt from '../../../libs/altCode';
+import alt from '../../libs/alt';
 import setup from './setup';
 setup(alt);
 export default ({children}) => <AltContainer flux={alt}> {children} </AltContainer>
